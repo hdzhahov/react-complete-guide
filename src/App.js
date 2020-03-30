9ae184b07caf5315344aa8227e9c1db5b2945e4f
Hristo Dzhahov <hdzhahov@gmail.com>
2020-03-30 22:24:21 +0300

    Add onChange handler and basic styling

--- src/App.js
+++ src/App.js
@@ -40,19 +40,48 @@ class App extends Component {
         });
     }
 
+	nameChangedHandler = (event) => {
+		this.setState({
+			persons: [
+				{
+					name: 'Hristo',
+					age: 25
+				},
+				{
+					name: event.target.value,
+					age: 22
+				},
+				{
+					name: 'Stephanie',
+					age: 24
+				}
+			]
+		});
+	}
+
     render() {
+        const style = {
+            backgroundColor: 'white',
+            font: 'inherit',
+            border: '1px solid blue',
+            padding: '8px',
+            cursor: 'pointer'
+        };
         return (
             <div className="App">
                 <h1>Hi, I'm React App</h1>
                 <p>This is really working</p>
-                <button onClick={() => this.switchNameHandler('Hristomir!!')}>Switch Name</button>
+                <button
+                    style={style}
+                    onClick={() => this.switchNameHandler('Hristomir!!')}>Switch Name</button>
                 <Person
                     name={this.state.persons[0].name}
                     age={this.state.persons[0].age} />
                 <Person
                     name={this.state.persons[1].name}
                     age={this.state.persons[1].age}
-                    click={this.switchNameHandler.bind(this, 'Hristo!!!')} >My Hobbies: Racing</Person>
+                    click={this.switchNameHandler.bind(this, 'Hristo!!!')}
+                    changed={this.nameChangedHandler}>My Hobbies: Racing</Person>
                 <Person
                     name={this.state.persons[2].name}
                     age={this.state.persons[2].age} />
